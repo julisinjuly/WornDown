15bde254ee9dd8f59c338c199933f61df3a01429
Julis <130704436+julisinjuly@users.noreply.github.com>
2025-10-08 21:28:41 +0300

    recompiling debug in process extra plugin removed

--- Plugins/MounteaDialogueSystem-master/Source/MounteaDialogueSystem/Private/WBP/MounteaDialogue.cpp
+++
@@ -1,9 +0,0 @@
-﻿// All rights reserved Dominik Morse (Pavlicek) 2024.
-
-
-#include "WBP/MounteaDialogue.h"
-
-UMounteaDialogue::UMounteaDialogue(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-	SetIsFocusable(true);
-}
